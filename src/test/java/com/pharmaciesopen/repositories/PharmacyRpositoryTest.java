75e0a3f09930063a408866942278dbcf432fae42
Imbabura <imbabura.apps@gmail.com>
2017-10-31 16:40:45 -0300

    finished test cases of repository

--- src/test/java/com/pharmaciesopen/repositories/PharmacyRpositoryTest.java
+++ src/test/java/com/pharmaciesopen/repositories/PharmacyRpositoryTest.java
@@ -3,6 +3,9 @@ package com.pharmaciesopen.repositories;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -21,13 +24,36 @@ public class PharmacyRpositoryTest {
 	private TestEntityManager testEntityManager;
 	@Autowired
 	private PharmacyRepository pharmacyRepository;
+	private Address addressMock, anotherAddressMock;
+	private Pharmacy pharmacy, anotherPharmacy;
+	
+	@Before
+	public void setUp() {
+		addressMock= Mockito.mock(Address.class);
+		anotherAddressMock= Mockito.mock(Address.class);
+		pharmacy= new Pharmacy("Pharmacy1", new ArrayList<String>(), addressMock);
+		anotherPharmacy= new Pharmacy("Pharmacy2", new ArrayList<String>(), anotherAddressMock);
+	}
 	
 	@Test
 	public void testGetPharmacyById() {
-		Address addressMock= Mockito.mock(Address.class);
-		Pharmacy persistedPharmacy= new Pharmacy("Pharmacy1", new ArrayList<String>(), addressMock);
-		testEntityManager.persist(persistedPharmacy);
-		Pharmacy pharmacy= pharmacyRepository.getPharmacyById(persistedPharmacy.getId());
-		assertTrue(persistedPharmacy.getName().equals(pharmacy.getName()));
+		testEntityManager.persist(pharmacy);
+		Pharmacy pharmacyResult= pharmacyRepository.getPharmacyById(pharmacy.getId());
+		assertTrue(pharmacy.getName().equals(pharmacyResult.getName()));
+	}
+	
+	@Test
+	public void testGetAllPharmacies() {
+		testEntityManager.persist(anotherPharmacy);
+		testEntityManager.persist(pharmacy);
+		List<Pharmacy> pharmacies= pharmacyRepository.getAllPharmacies();
+		assertTrue(pharmacies.size() == 2);
+	}
+	
+	@Test
+	public void testAddPharmacy() {
+		pharmacyRepository.addPharmacy(pharmacy);
+		Pharmacy pharmacyResult= pharmacyRepository.getPharmacyById(pharmacy.getId());
+		assertTrue(pharmacy.getName().equals(pharmacyResult.getName()));
 	}
 }
