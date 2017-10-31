b960a79c9e535a910a0731102af8498221945fec
Imbabura <imbabura.apps@gmail.com>
2017-10-31 16:56:13 -0300

    finished test cases of PharmacyService

--- src/test/java/com/pharmacies/services/PharmacyServiceTest.java
+++ src/test/java/com/pharmacies/services/PharmacyServiceTest.java
@@ -1,7 +1,12 @@
 package com.pharmacies.services;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -29,5 +34,20 @@ public class PharmacyServiceTest {
 	public void testGetPharmacyById() {
 		when(pharmacyRepository.getPharmacyById(1)).thenReturn(pharmacyMock);
 		assertEquals(pharmacyMock, pharmacyService.getPharmacyById(1));
+	}
+	
+	@Test
+	public void testGetAllPharmacies() {
+		List<Pharmacy> pharmacies= new ArrayList<Pharmacy>();
+		pharmacies.add(pharmacyMock);
+		when(pharmacyRepository.getAllPharmacies()).thenReturn(pharmacies);
+		assertEquals(1, pharmacyService.getAllPharmacies().size());
+		assertEquals(pharmacyMock, pharmacyService.getAllPharmacies().get(0));
+	}
+	
+	@Test
+	public void testAddPharmacy() {
+		pharmacyService.addPharmacy(pharmacyMock);
+		verify(pharmacyRepository, times(1)).addPharmacy(pharmacyMock);
 	}
 }
